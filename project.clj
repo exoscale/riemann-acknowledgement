55596c28b1e3fafb230b3c45eb575e46c2de5506
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-12-10 16:17:42 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/exoscale/riemann-acknowledgement"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure       "1.5.1"]
-                 [compojure                 "1.1.6"]
-                 [ring/ring-json            "0.2.0"]
+  :dependencies [[org.clojure/clojure       "1.6.0"]
+                 [compojure                 "1.3.1"]
+                 [ring/ring-json            "0.3.1"]
                  [javax.servlet/servlet-api "2.5"]
-                 [riemann                   "0.2.4"
-                  :exclusions [org.slf4j/slf4j-log4j12]]])
+                 [riemann                   "0.2.6"
+                  :exclusions [org.slf4j/slf4j-log4j12 joda-time]]])
